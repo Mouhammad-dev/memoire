8692d6321a9f6d08dad2b786f1ff1dda63c41b15
cheikh0101 <ctidiane.gueye1@univ-thies.sn>
2022-10-03 10:58:09 +0000

    amelioration de backpack

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -12,7 +12,5 @@
         Patients</a></li>
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('dossier') }}"><i class="nav-icon la la-question"></i>
         Dossiers</a></li>
-<li class="nav-item"><a class="nav-link" href="{{ backpack_url('consultation') }}"><i
-            class="nav-icon la la-question"></i> Consultations</a></li>
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('rendez-vou') }}"><i class="nav-icon la la-question"></i>
         Rendez vous</a></li>
