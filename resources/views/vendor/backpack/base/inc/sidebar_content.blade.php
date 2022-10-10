551c477f772dc7ac3a343cfdcfb61109c1e51687
cheikh0101 <ctidiane.gueye1@univ-thies.sn>
2022-10-10 15:56:08 +0000

    gestion de la vue

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -1,15 +1,26 @@
 {{-- This file is used to store sidebar items, inside the Backpack admin panel --}}
-<li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i>
-        {{ trans('backpack::base.dashboard') }}</a></li>
-<li class="nav-item"><a class="nav-link" href="{{ backpack_url('pathologie') }}"><i class="nav-icon la la-dna"></i>
-        Pathologies</a></li>
-<li class="nav-item"><a class="nav-link" href="{{ backpack_url('medicament') }}"><i class="nav-icon la la-tablets"></i>
-        Medicaments</a></li>
-<li class="nav-item"><a class="nav-link" href="{{ backpack_url('user') }}"><i class="nav-icon la la-users"></i>
-        Utilisateurs</a></li>
-<li class="nav-item"><a class="nav-link" href="{{ backpack_url('patient') }}"><i class="nav-icon la la-briefcase-medical"></i>
-        Patients</a></li>
-<li class="nav-item"><a class="nav-link" href="{{ backpack_url('dossier') }}"><i class="nav-icon la la-book-medical"></i>
-        Dossiers</a></li>
-<li class="nav-item"><a class="nav-link" href="{{ backpack_url('rendez-vou') }}"><i class="nav-icon la la-calendar"></i>
-        Rendez Vous</a></li>
+@if (backpack_user()->type == 'admin')
+    <li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i>
+            {{ trans('backpack::base.dashboard') }}</a></li>
+    <li class="nav-item"><a class="nav-link" href="{{ backpack_url('pathologie') }}"><i class="nav-icon la la-dna"></i>
+            Pathologies</a></li>
+    <li class="nav-item"><a class="nav-link" href="{{ backpack_url('medicament') }}"><i
+                class="nav-icon la la-tablets"></i>
+            Medicaments</a></li>
+    <li class="nav-item"><a class="nav-link" href="{{ backpack_url('user') }}"><i class="nav-icon la la-users"></i>
+            Utilisateurs</a></li>
+    <li class="nav-item"><a class="nav-link" href="{{ backpack_url('patient') }}"><i
+                class="nav-icon la la-briefcase-medical"></i>
+            Patients</a></li>
+    <li class="nav-item"><a class="nav-link" href="{{ backpack_url('rendez-vou') }}"><i
+                class="nav-icon la la-calendar"></i>
+            Rendez Vous</a></li>
+@endif
+@if (backpack_user()->type == 'docteur')
+    <li class="nav-item"><a class="nav-link" href="{{ backpack_url('dossier') }}"><i
+                class="nav-icon la la-book-medical"></i>
+            Dossiers</a></li>
+    <li class="nav-item"><a class="nav-link" href="{{ backpack_url('consultation') }}"><i
+                class="nav-icon la la-book-medical"></i>
+            Consultations</a></li>
+@endif
