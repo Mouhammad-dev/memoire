5b95be0ae46c52d67b74ce19e9b9d7c19ee84d01
cheikh0101 <ctidiane.gueye1@univ-thies.sn>
2022-10-14 12:06:22 +0000

    simple commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,10 +1,23 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
+    <div class="container mt-5">
         <div class="row justify-content-center">
-            <div class="col-md-8">
-
+            <div class="col-md-6">
+                <div class="card" style="width: 18rem;">
+                    <img src="../../images/heart-attack.png" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <p class="card-text">Consultation: 10</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="card" style="width: 18rem;">
+                    <img src="../../images/heart-attack.png" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <p class="card-text">Rendez-vous: 20</p>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -42,13 +42,15 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav me-auto">
-                        <li class="nav-item">
-                            <a class="nav-link active"
-                                href=" {{ route('consultation.index') }} ">{{ __('Mes consultations') }}</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link active" href=" {{ route('rv.index') }} ">{{ __('Mes rv') }}</a>
-                        </li>
+                        @auth
+                            <li class="nav-item">
+                                <a class="nav-link active"
+                                    href=" {{ route('consultation.index') }} ">{{ __('Mes consultations') }}</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link active" href=" {{ route('rv.index') }} ">{{ __('Mes rv') }}</a>
+                            </li>
+                        @endauth
                     </ul>
 
                     <!-- Right Side Of Navbar -->
